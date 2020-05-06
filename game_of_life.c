34e38c6937d81c7316dfda7d7c92d8a5d8bd740c
James <jamesdenney2@gmail.com>
2020-05-06 10:04:28 -0500

    Migrated the MPI variant from GitLab

--- game_of_life.c
+++ game_of_life.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <stdbool.h>
-#include <omp.h>
+#include <mpi.h>
 
 /*
 Name: James Denney
 BlazerId: jdenney2
 Course Section: CS 432
-Homework #: 3
+Homework #: 4
 */
 
 /*
@@ -21,7 +21,6 @@ To Run: Type ./<program-name> <X-Dimension> <Y-Dimension> <Number-of-Generations
 */
 
 //Used to keep track of whether or not an iteration of the board has changed.
-bool changed = false;
 
 double gettime(void) {
 	struct timeval tval;
@@ -84,40 +83,6 @@ bool **copyGhostCells(bool **a, int x, int y) {
 	return a;
 }
 
-//initializes the temporary array, which stores whether a value has changed or not
-bool **initTempArray(bool **a, int x, int y) {
-	int i, j;
-
-	for(i = 1; i < y - 1; i++) {
-		for(j = 1; j < x - 1; j++) {
-			a[i][j] = false;
-		}
-	}
-	return a;
-}
-
-//Compares the main array with the temp array and makes the appropriate changes.
-//Important note: The temp array does NOT store real values. Its purpose is to store whether or not a value should change.
-bool **compareArrays(bool **realArr, bool **compArr, int x, int y) {
-	int i, j;
-
-	for(i = 0; i < y; i++) {
-		for(j = 0; j < x; j++) {
-			if (compArr[i][j]) {
-				changed = true;
-				if(realArr[i][j]) {
-					realArr[i][j] = false;
-				}
-				else {
-					realArr[i][j] = true;
-				}
-				compArr[i][j] = false;
-			}
-		}
-	}
-	return realArr;
-}
-
 //Prints a 2d array
 void printarray(bool **a, int x, int y) {
 	int i, j;
@@ -132,14 +97,11 @@ void printarray(bool **a, int x, int y) {
 }
 
 //Check all of the "real" cell's neighbors, and stores whether or not they should live in a temp array
-bool **checkNeighbors(bool **arr,bool **tempArr, int x, int y, int numThreads) {
+bool **checkNeighbors(bool **arr,bool **tempArr, int size, int start, int end) {
 	int i, j, count;
-	changed = false;
 
-	#pragma omp parallel for num_threads(numThreads) private(i, j, count)
-
-	for(i = 1; i < y - 1; i++) {
-		for(j = 1; j < x - 1; j++) {
+	for(i = start; i < end; i++) {
+		for(j = 1; j < size - 1; j++) {
 			count = 0;
 			//printf("Checking (%d,%d):\n", j, i);
 	
@@ -154,68 +116,111 @@ bool **checkNeighbors(bool **arr,bool **tempArr, int x, int y, int numThreads) {
    
 			//Die if lonely
 			if(count <= 1 && arr[i][j]) {
-				tempArr[i][j] = true;
-				changed = true;
+				tempArr[i][j] = false;
 			}
 
 			//Die if overcrowded
 			if(count >= 4 && arr[i][j]) {
-				tempArr[i][j] = true;
-				changed = true;
+				tempArr[i][j] = false;
 			}	
 
 			//Return to life
 			if(count == 3 && !arr[i][j]) {
 				tempArr[i][j] = true;
-				changed = true;
 			}
 		}
 	}
 	
-	return copyGhostCells(tempArr, x, y);
+	return tempArr;
+}
 
+bool **consolodateArrs(bool **a, bool **b, int size, int start, int end) {
+	int i, j;
+	for (i = start; i < end; i++) { 
+		for (j = 1; j < size - 1; j++) {
+			a[i][j] = b[i][j];
+		}
+	}
+	return a;	
 }
 
 //Queues up a set of tests, defined by user input.
 int main (int argc, char **argv) {
-	int x, y, ngen, i, j;
+	int size, ngen, i, j;
 	double starttime, endtime;
 
 	srand48(time(0));
 
-	if (argc != 5) {
-		printf("Usage: <%s> <X-Dimension> <Y-Dimension> <N-Generations> <N-Threads>\n", argv[0]);
+	if (argc != 3) {
+		printf("Usage: <%s> <Size> <N-Generations>\n", argv[0]);
 		exit(-1);
 	}
 
-	x = atoi(argv[1]) + 2;
-	y = atoi(argv[2]) + 2;
-	ngen = atoi(argv[3]);
+	size = atoi(argv[1]) + 2;
+	ngen = atoi(argv[2]);	 	
 
 	bool **a = NULL, **b = NULL;		
 
-	a = allocarray(x, y);
-	b = allocarray(x, y);
+	a = allocarray(size, size);
+	b = allocarray(size, size);
 
-	a = initarray(a, x, y);
-	b = initTempArray(b, x, y);
-	a = copyGhostCells(a, x, y);
-	b = copyGhostCells(b, x, y);
+	a = initarray(a, size, size);
+	a = copyGhostCells(a, size, size);
 
 	starttime = gettime();	
-	//printarray(a, x, y);		
-	
-	for(j = 0; j < ngen; j++) {
-		b = checkNeighbors(a, b, x, y, atoi(argv[4]));
-		if(!changed) {
-			break;
-		}
-		a = compareArrays(a, b, x, y);
-		//printarray(a, x, y);
+	//printarray(a, size, size);		
+
+	MPI_Init(NULL, NULL);
+
+	int worldRank, worldSize;	
+	MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
+	MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+
+	int start = (size/worldSize*worldRank);
+	if (start == 0) {
+		start = 1;
 	}
 
-	endtime = gettime();
-	printf("Time taken for test = %lf seconds\n", endtime-starttime);
+	int end = (size/worldSize*(worldRank+1));
+	if (end >= size - 1) {
+		end = size - 1;
+	}
 
-	return 0;
+	for(j = 0; j < ngen; j++) {
+		b = checkNeighbors(a, b, size, start, end);
+		if (worldRank != 0) {
+			MPI_Send(&(b[0][0]), size, MPI_C_BOOL, 0, 0, MPI_COMM_WORLD);
+			MPI_Recv(&(a[0][0]), size, MPI_C_BOOL, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+		}
+		else {
+			a = consolodateArrs(a, b, size, start, end);
+			for (i = 1; i < worldSize; i++) {
+				MPI_Recv(&(b[0][0]), size, MPI_C_BOOL, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+				
+				int iStart = (size/worldSize*i);
+			        if (iStart == 0) {
+                			iStart = 1;
+        			}
+				int iEnd = (size/worldSize*(i+1));
+				if (iEnd > size - 1) {
+					iEnd = size - 1;
+				}
+ 				
+				a = consolodateArrs(a, b, size, iStart, iEnd);
+			}
+			
+			a = copyGhostCells(a, size, size);
+			//printarray(a, size, size);			
+
+			for (i = 1; i < worldSize; i++) {
+				MPI_Send(a, size, MPI_C_BOOL, i, 0, MPI_COMM_WORLD);
+			}
+		}		
+	}
+	if (worldRank == 0) { 
+		endtime = gettime();
+		printf("Time taken for test = %lf seconds\n", endtime-starttime);
+	}
+	
+	MPI_Finalize();
 }
